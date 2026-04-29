058160a7329cc62b3bd6c2a22f522b360ce592f7
Leando M. Peralta <argenkiwi@hotmail.com>
2026-04-30 10:27:48 +1200

    Include global agents settings in container.

--- container.sh
+++ container.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 podman build -t ambler.ts . && \
-podman run -v $(pwd):/app:z -e OLLAMA_HOST=$OLLAMA_HOST --name ambler.ts --replace -it ambler.ts
+podman run -v ~/.agents:/root/.agents:z -v $(pwd):/app:z -e OLLAMA_HOST=$OLLAMA_HOST --name ambler.ts --replace -it ambler.ts
